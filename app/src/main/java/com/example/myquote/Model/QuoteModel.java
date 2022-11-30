5a1cfc25621da6562eac719f9c61bb85b6a59661
NathaciaLais <nathacialais@gmail.com>
2022-11-30 17:02:34 +0700

    new

---
+++ app/src/main/java/com/example/myquote/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.example.myquote.Model;
+
+public class QuoteModel {
+    private String text,author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
