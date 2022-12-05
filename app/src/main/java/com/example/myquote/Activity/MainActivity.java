bbe6c5070e7a3db3bd265ae7370742e88c75d31c
NathaciaLais <nathacialais@gmail.com>
2022-12-05 15:08:02 +0700

    UPDATE 5.0

--- app/src/main/java/com/example/myquote/Activity/MainActivity.java
+++ app/src/main/java/com/example/myquote/Activity/MainActivity.java
@@ -1,16 +1,63 @@
 package com.example.myquote.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.myquote.API.APIRequestData;
+import com.example.myquote.API.RetroServer;
+import com.example.myquote.Adapter.AdapterQuote;
+import com.example.myquote.Model.QuoteModel;
 import com.example.myquote.R;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvQuote;
+    private ProgressBar pbQuote;
+    private List<QuoteModel> ListQuote;
+    private AdapterQuote adQuote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvQuote = findViewById(R.id.rw_Qoute);
+        pbQuote = findViewById(R.id.pb_quote);
+        rvQuote.setLayoutManager(new LinearLayoutManager(this));
+        retrieveData();
+    }
+    private void retrieveData(){
+//        membangun hubungan
+        pbQuote.setVisibility(View.VISIBLE);
+        APIRequestData ARD = RetroServer.connectRetrofit().create(APIRequestData.class);
+        Call<List<QuoteModel>> retrieveProcess = ARD.ardRetrieve();
+
+        retrieveProcess.enqueue(new Callback<List<QuoteModel>>() {
+            @Override
+            public void onResponse(Call<List<QuoteModel>> call, Response<List<QuoteModel>> response) {
+                ListQuote = response.body();
+                adQuote = new AdapterQuote(ListQuote,MainActivity.this);
+                rvQuote.setAdapter(adQuote);
+                pbQuote.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<QuoteModel>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Connection Error! Cannot Reach Server!", Toast.LENGTH_SHORT).show();
+                pbQuote.setVisibility(View.GONE);
+
+            }
+        });
     }
 }
